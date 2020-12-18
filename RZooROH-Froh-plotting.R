657c6e43710c913e38f0c9a6d41bab4a414c2367
cziyani <76186315+cziyani@users.noreply.github.com>
2020-12-18 17:32:44 +0100

    Update RZooROH-Froh-plotting.R

--- RZooROH-Froh-plotting.R
+++ RZooROH-Froh-plotting.R
@@ -1,7 +1,7 @@
 ####plotting 
 
 #Froh plot :
-setwd("C:/Users/chaym/Desktop/FSP/firststep/RzooROH/Nouveau dossier/")
+setwd("./")
 
 ##Read the file
 df <- read.table("my_final_F_Rzoo_out", header = T)
@@ -9,11 +9,11 @@ df <- read.table("my_final_F_Rzoo_out", header = T)
 ##define RAD_FRAG numbers levels 
 df$RAD_Frag <- factor(df$RAD_Frag , levels = c(120000 ,140000 , 160000))
 
-##have the plot on the same row in different columns
+##have the plot on the same row and different columns
 par(mfrow=c(1,3))
+
 ## loop through RAD_FRAG levels
 for (i in levels(df$RAD_Frag)){
-  ##List all the var and store them in one list
   df_lev <- df[df$RAD_Frag == i ,]
   ##calculate the mean each levels of fragment depending on the SIM_ID and ID for each individulate for WGS and RAD_sub
   F_mean_sub <- aggregate(df_lev$KB_RAD , by = list(df_lev$Sim_Id , df_lev$ID) , mean)
